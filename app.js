1834784599056fb81091ec58eb9ac1ea8ee44118
Teerawut <t.kitbunjerdjarud@gmail.com>
2019-10-04 15:40:18 +0700

    update

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
+require("dotenv").config();
 const line = require("@line/bot-sdk");
 const express = require("express");
 
 // create LINE SDK config from env variables
 const config = {
-  channelAccessToken:
-    "3UnjnmWz+x6LRsArfDj8Gzy6aBddcRIWPJIgtCMstQXdAnyqDDmbWfI7Qi4NJbUSUN8d490IIV+rwIUvjjE44U7RMnNQWGpOLnCLC2uP/h9KVah9fnch+NHERIK7y1/UE1xIaLTQRc/doP95W6ux5wdB04t89/1O/w1cDnyilFU=",
-  channelSecret: "2f02b59173d521aeeb8169caaeb5e0d3"
+  channelAccessToken: process.env.TOKEN,
+  channelSecret: process.env.SECRET
 };
 
 // create LINE SDK client
@@ -17,7 +17,7 @@ const app = express();
 
 // register a webhook handler with middleware
 // about the middleware, please refer to doc
-app.post("/callback", line.middleware(config), (req, res) => {
+app.post("/webhook", line.middleware(config), (req, res) => {
   Promise.all(req.body.events.map(handleEvent))
     .then(result => res.json(result))
     .catch(err => {
@@ -39,106 +39,86 @@ function handleEvent(event) {
   }
 
   // create a echoing text message
-  const echo = {
-    type: "flex",
-    contents: MessageHandler(event.message.text)
-  };
+  const echo = messageHandler(event.message.text);
 
   // use reply API
   return client.replyMessage(event.replyToken, echo);
 }
 
+// create flex bubble
 function createBubble(amount, category) {
   const date = new Date().toJSON().split("T")[0];
   const data = {
-    type: "bubble",
-    styles: {
-      header: {},
-      body: {},
-      footer: {}
-    },
-    header: {
-      type: "box",
-      layout: "vertical",
-      contents: [
-        {
-          type: "text",
-          text: "TRACK EXPENSE",
-          size: "lg",
-          weight: "bold",
-          color: "#1DB446",
-          align: "start"
-        }
-      ]
-    },
-    body: {
-      type: "box",
-      layout: "vertical",
-      contents: [
-        {
-          type: "text",
-          text: `${amount}`,
-          size: "xxl",
-          weight: "bold",
-          color: "#000000"
-        },
-        {
-          type: "text",
-          text: `${category}`,
-          size: "lg",
-          weight: "bold",
-          color: "#000000"
-        },
-        {
-          type: "text",
-          text: `${date}`,
-          size: "xs",
-          color: "#B2B2B2"
-        }
-      ]
-    },
-    footer: {
-      type: "box",
-      layout: "horizontal",
-      margin: "lg",
-      contents: [
-        {
-          type: "text",
-          text: "Total",
-          size: "md",
-          weight: "bold",
-          color: "#0084B6",
-          align: "start"
-        },
-        {
-          type: "text",
-          text: "฿500",
-          color: "#000000",
-          size: "md",
-          weight: "bold",
-          align: "end"
-        }
-      ]
+    type: "flex",
+    altText: `฿${amount} used by ${category}`,
+    contents: {
+      type: "bubble",
+      body: {
+        type: "box",
+        layout: "vertical",
+        contents: [
+          {
+            type: "text",
+            text: "TRACK EXPENSE",
+            weight: "bold",
+            color: "#1DB446",
+            size: "lg"
+          },
+          {
+            type: "text",
+            text: `฿${amount}`,
+            weight: "bold",
+            size: "xxl",
+            margin: "md"
+          },
+          {
+            type: "text",
+            text: `${category}`,
+            size: "md",
+            color: "#aaaaaa",
+            wrap: true
+          },
+          {
+            type: "separator",
+            margin: "sm"
+          },
+          {
+            type: "box",
+            layout: "horizontal",
+            margin: "md",
+            contents: [
+              {
+                type: "text",
+                text: `${date}`,
+                color: "#aaaaaa",
+                size: "xs",
+                align: "end"
+              }
+            ]
+          }
+        ]
+      }
     }
   };
-  console.log(data);
   return data;
 }
 
-function MessageHandler(message) {
+// message handler
+function messageHandler(message) {
   let match;
-  if ((match = message.match(/^[\d.]+?[tfghmol]$/i))) {
+  if ((match = message.match(/^([\d.]+)([tfghmol])$/i))) {
     const m = match;
-    const amount = +m[0];
+    const enteredAmount = +m[1];
+    const amount = enteredAmount.toFixed(2);
     const category = {
-      t: "transportation",
-      f: "food",
-      g: "game",
-      h: "health",
-      m: "miscellaneous",
-      o: "occasion",
-      l: "lodging"
-    }[m[1].toLowerCase()];
+      t: "Transportation",
+      f: "Food",
+      g: "Game",
+      h: "Health",
+      m: "Miscellaneous",
+      o: "Occasion",
+      l: "Lodging"
+    }[m[2].toLowerCase()];
     return createBubble(amount, category);
   }
 }
